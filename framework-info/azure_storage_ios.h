ee7b3e8aa34d7f52b159134e1c20c152f6e3f426
Jelle Alten <>
2019-02-07 16:25:40 +0100

    empty framework

---
+++ framework-info/azure_storage_ios.h
@@ -0,0 +1,19 @@
+//
+//  azure_storage_ios.h
+//  azure-storage-ios
+//
+//  Created by Jelle Alten on 07/02/2019.
+//  Copyright © 2019 framework. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for azure_storage_ios.
+FOUNDATION_EXPORT double azure_storage_iosVersionNumber;
+
+//! Project version string for azure_storage_ios.
+FOUNDATION_EXPORT const unsigned char azure_storage_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <azure_storage_ios/PublicHeader.h>
+
+
