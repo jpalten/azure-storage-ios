a0ffb6c5880442fbfef72447002b39ee5afb476d
Jelle Alten <>
2019-02-08 16:59:21 +0100

    make all headers public

--- framework-info/AzureStorage.h
+++ framework-info/AzureStorage.h
@@ -15,10 +15,62 @@ FOUNDATION_EXPORT const unsigned char azure_storage_iosVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <azure_storage_ios/PublicHeader.h>
 
-#import <AzureStorage/AZSCloudStorageAccount.h>
-#import <AzureStorage/AZSCloudBlobClient.h>
-#import <AzureStorage/AZSCloudBlobContainer.h>
-#import <AzureStorage/AZSEnums.h>
-#import <AzureStorage/AZSCloudBlockBlob.h>
+#import <AzureStorage/ZSAccessCondition.h>
+#import <AzureStorage/ZSAuthenticationHandler.h>
+#import <AzureStorage/ZSBlobContainerProperties.h>
+#import <AzureStorage/ZSBlobOutputStream.h>
+#import <AzureStorage/ZSBlobProperties.h>
+#import <AzureStorage/ZSBlobRequestFactory.h>
+#import <AzureStorage/ZSBlobRequestOptions.h>
+#import <AzureStorage/ZSBlobRequestXML.h>
+#import <AzureStorage/ZSBlobResponseParser.h>
+#import <AzureStorage/ZSBlobUploadHelper.h>
+#import <AzureStorage/ZSBlockListItem.h>
+#import <AzureStorage/ZSClient.h>
+#import <AzureStorage/ZSCloudAppendBlob.h>
+#import <AzureStorage/ZSCloudBlob.h>
+#import <AzureStorage/ZSCloudBlobClient.h>
+#import <AzureStorage/ZSCloudBlobContainer.h>
+#import <AzureStorage/ZSCloudBlobDirectory.h>
+#import <AzureStorage/ZSCloudBlockBlob.h>
+#import <AzureStorage/ZSCloudClient.h>
+#import <AzureStorage/ZSCloudPageBlob.h>
+#import <AzureStorage/ZSCloudStorageAccount.h>
+#import <AzureStorage/ZSConstants.h>
+#import <AzureStorage/ZSContinuationToken.h>
+#import <AzureStorage/ZSCopyState.h>
+#import <AzureStorage/ZSCorsRule.h>
+#import <AzureStorage/ZSEnums.h>
+#import <AzureStorage/ZSErrors.h>
+#import <AzureStorage/ZSExecutor.h>
+#import <AzureStorage/ZSIPRange.h>
+#import <AzureStorage/ZSLoggingProperties.h>
+#import <AzureStorage/ZSMacros.h>
+#import <AzureStorage/ZSMetricsProperties.h>
+#import <AzureStorage/ZSNavigationUtil.h>
+#import <AzureStorage/ZSNoOpAuthenticationHandler.h>
+#import <AzureStorage/ZSOperationContext.h>
+#import <AzureStorage/ZSRequestFactory.h>
+#import <AzureStorage/ZSRequestOptions.h>
+#import <AzureStorage/ZSRequestResult.h>
+#import <AzureStorage/ZSResponseParser.h>
+#import <AzureStorage/ZSResultSegment.h>
+#import <AzureStorage/ZSRetryContext.h>
+#import <AzureStorage/ZSRetryInfo.h>
+#import <AzureStorage/ZSRetryPolicy.h>
+#import <AzureStorage/ZSServiceProperties.h>
+#import <AzureStorage/ZSSharedAccessAccountParameters.h>
+#import <AzureStorage/ZSSharedAccessBlobParameters.h>
+#import <AzureStorage/ZSSharedAccessHeaders.h>
+#import <AzureStorage/ZSSharedAccessPolicy.h>
+#import <AzureStorage/ZSSharedAccessSignatureHelper.h>
+#import <AzureStorage/ZSSharedKeyBlobAuthenticationHandler.h>
+#import <AzureStorage/ZSStorageCommand.h>
+#import <AzureStorage/ZSStorageCredentials.h>
+#import <AzureStorage/ZSStorageUri.h>
+#import <AzureStorage/ZSULLRange.h>
+#import <AzureStorage/ZSUriQueryBuilder.h>
+#import <AzureStorage/ZSUtil.h>
+
 
 #import <libxml/xmlwriter.h>
